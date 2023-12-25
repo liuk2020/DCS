229936fd7e419a1902091a0cd7831fccd13aefa6
liuk2020 <lk2020@mail.ustc.edu.cn>
2023-12-25 16:07:55 +0800

    feat: plot B contour

--- dcs/localEquilibrium/straightSurface.py
+++ dcs/localEquilibrium/straightSurface.py
@@ -56,6 +56,40 @@ class StraightSurfaceField:
         )
         return np.power(B2Grid, 0.5)
 
+    def plotB(self, ntheta: int=360, nzeta: int=360, ax=None, fig=None, onePeriod: bool=True, **kwargs):
+        from matplotlib import cm
+        import matplotlib.pyplot as plt 
+        thetaArr = np.linspace(0, 2*np.pi, ntheta)
+        thetaValue =  np.linspace(0, 2*np.pi, 3)
+        if onePeriod:
+            zetaArr = np.linspace(0, 2*np.pi/self.nfp, nzeta)
+            zetaValue =  np.linspace(0, 2*np.pi/self.nfp, 3)
+        else:
+            zetaArr = np.linspace(0, 2*np.pi, nzeta) 
+            zetaValue =  np.linspace(0, 2*np.pi, 3)
+        if ax is None: 
+            fig, ax = plt.subplots() 
+        plt.sca(ax) 
+        thetaGrid, zetaGrid = np.meshgrid(thetaArr, zetaArr) 
+        valueGrid = self.getB(thetaGrid, zetaGrid) 
+        ctrig = ax.contourf(zetaGrid, thetaGrid, valueGrid, cmap=cm.rainbow)
+        colorbar = fig.colorbar(ctrig)
+        colorbar.ax.tick_params(labelsize=18)
+        if kwargs.get("toroidalLabel") == None:
+            kwargs.update({"toroidalLabel": r"$\zeta$"})
+        if kwargs.get("poloidalLabel") == None:
+            kwargs.update({"poloidalLabel": r"$\theta$"})
+        ax.set_xlabel(kwargs.get("toroidalLabel"), fontsize=18)
+        ax.set_ylabel(kwargs.get("poloidalLabel"), fontsize=18)
+        ax.set_xticks(zetaValue)
+        if onePeriod and self.nfp!=1:
+            ax.set_xticklabels(["$0$", r"$\pi/"+str(self.nfp)+"$", r"$2\pi/"+str(self.nfp)+"$"], fontsize=18) 
+        else:
+            ax.set_xticklabels(["$0$", r"$\pi$", r"$2\pi$"], fontsize=18) 
+        ax.set_yticks(thetaValue)
+        ax.set_yticklabels(["$0$", r"$\pi$", r"$2\pi$"], fontsize=18)
+        return
+
     def solveJacobian(self) -> None:
         matrixCoef = self.getMatrixCoef() 
         vectorB = self.getVectorB()
